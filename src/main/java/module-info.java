2400b275b468e954dde0ce5ed6b57cc6d7c27ce2
Jean-Santoni <santonigs.2a@gmail.com>
2023-02-28 10:25:05 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.miniprojet3memory {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.miniprojet3memory to javafx.fxml;
+    exports com.example.miniprojet3memory;
+}
