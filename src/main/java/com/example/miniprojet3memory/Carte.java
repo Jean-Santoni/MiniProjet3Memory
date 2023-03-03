5d5b791148776d017c4e400218f6ec3184fa26b2
Jean-Santoni <santonigs.2a@gmail.com>
2023-03-03 15:56:27 +0100

    commit 3/3/23 jeu en état de marche

--- src/main/java/com/example/miniprojet3memory/Carte.java
+++ src/main/java/com/example/miniprojet3memory/Carte.java
@@ -5,12 +5,41 @@ import javafx.scene.image.ImageView;
 
 public class Carte {
 
-    private Image dos = new Image("file:ressources/téléchargement.png",240,240,false,false);
+    private Image dos = new Image("file:resources/3il.png",240,240,false,false);
+    private Image valide = new Image("file:resources/téléchargement.png",240,240,false,false);
     private Image face ;
+
+    private boolean clique = false;
+    private boolean validation = false;
+
 
     public Carte(Image image){
         this.face=image;
 
+    }
+    public Image getDos(){
+        return this.dos;
+    }
+    public Image getValide(){
+        return this.valide;
+    }
+    public Image getFace(){
+        return this.face;
+    }
+    public void CarteClique(){
+        this.clique=true;
+    }
+    public void carteEstValide(){
+        this.validation=true;
+    }
+    public void CarteFausse(){
+        this.clique=false;
+    }
+    public boolean isClique(){
+        return this.clique;
+    }
+    public boolean isValide(){
+        return this.validation;
     }
 
 
--- src/main/java/com/example/miniprojet3memory/CarteController.java
+++ src/main/java/com/example/miniprojet3memory/CarteController.java
@@ -10,50 +10,145 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Delayed;
+
+import static java.util.Collections.shuffle;
+
 public class CarteController {
+    private int nbClique=0;
+    private int nbCarteVailde=0;
+    @FXML
+    private ArrayList<Carte> ListeCarte = new ArrayList<>();
+    @FXML
+    private Carte CartePrecedent ;
+    @FXML
+    private Carte CarteActuel ;
     @FXML
     private Label titre;
+    @FXML
+    private Label partieFinie;
     @FXML
     private GridPane plateau;
     @FXML
     private ImageView photo;
 
+    @FXML
+    protected void InitialisationJeu(MouseEvent event) {
+        InitialisationListe();
+        InitialisationTableau();
+    }
+    @FXML
+    protected void InitialisationListe() {
+        ListeCarte.clear();
+        Carte m1 = new Carte(new Image("file:resources/c.png", 240, 240, false, false));
+        Carte m2 = new Carte(new Image("file:resources/java.png", 240, 240, false, false));
+        Carte m3 = new Carte(new Image("file:resources/chinois.png", 240, 240, false, false));
+        Carte m4 = new Carte(new Image("file:resources/Electricité.png", 240, 240, false, false));
+        Carte m5 = new Carte(new Image("file:resources/anglais.png", 240, 240, false, false));
+        Carte m6 = new Carte(new Image("file:resources/maths.png", 240, 240, false, false));
+        Carte m7 = new Carte(new Image("file:resources/c.png", 240, 240, false, false));
+        Carte m8 = new Carte(new Image("file:resources/java.png", 240, 240, false, false));
+        Carte m9 = new Carte(new Image("file:resources/chinois.png", 240, 240, false, false));
+        Carte m10 = new Carte(new Image("file:resources/Electricité.png", 240, 240, false, false));
+        Carte m11 = new Carte(new Image("file:resources/anglais.png", 240, 240, false, false));
+        Carte m12 = new Carte(new Image("file:resources/maths.png", 240, 240, false, false));
+
+        ListeCarte.add(m1);
+        ListeCarte.add(m2);
+        ListeCarte.add(m3);
+        ListeCarte.add(m4);
+        ListeCarte.add(m5);
+        ListeCarte.add(m6);
+        ListeCarte.add(m7);
+        ListeCarte.add(m8);
+        ListeCarte.add(m9);
+        ListeCarte.add(m10);
+        ListeCarte.add(m11);
+        ListeCarte.add(m12);
+        shuffle(ListeCarte);
+
+    }
+
+
 
 
     @FXML
-    protected void InitialisationTableau(MouseEvent event){
+    protected void InitialisationTableau() {
 
         plateau.getColumnConstraints().clear();
         plateau.getRowConstraints().clear();
-        for (int c =0 ; c <= 2 ;c++){
-            ColumnConstraints column = new ColumnConstraints( );
-            column.setPercentWidth(100.0/2);
+        for (int c = 0; c <= 2; c++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(100.0 / 2);
             plateau.getColumnConstraints().add(column);
         }
-        for (int r =0 ; r <= 3 ;r++){
-            RowConstraints row = new RowConstraints( );
-            row.setPercentHeight(100.0/3);
+        for (int r = 0; r <= 3; r++) {
+            RowConstraints row = new RowConstraints();
+            row.setPercentHeight(100.0 / 3);
             plateau.getRowConstraints().add(row);
         }
-        int collIndex =0;
+        int collIndex = 0;
         int rowIndex = 0;
-        for(collIndex=0;collIndex<=2;collIndex++){
-            for (rowIndex=0;rowIndex<=3;rowIndex++){
-                Button b = new Button();
-                b.setText(";sfkdjhfl;khjqdfvgs;kdh");
-                plateau.add(b,collIndex,rowIndex);
+        int listeIndex = 0;
+        for (collIndex = 0; collIndex <= 2; collIndex++) {
+            for (rowIndex = 0; rowIndex <= 3; rowIndex++) {
+                ImageView n = new ImageView();
+                if(ListeCarte.get(listeIndex).isClique()==false) {
+                    n.setImage(ListeCarte.get(listeIndex).getDos());
+                }else if(ListeCarte.get(listeIndex).isValide() && ListeCarte.get(listeIndex).isClique() ){
+                    n.setImage(ListeCarte.get(listeIndex).getValide());
+                }else {
+                    n.setImage(ListeCarte.get(listeIndex).getFace());
+                }
+                n.setFitWidth(100.0);
+                n.setFitHeight(100.0);
+                Carte carteTampon = ListeCarte.get(listeIndex);
+                n.setOnMouseClicked(event ->CliqueCase(carteTampon) );
+                plateau.add(n, collIndex, rowIndex);
+                listeIndex++;
             }
         }
-       // System.out.println(photo);
-       // Image i = new Image("file:ressources/dog.JPG",240,240,false,false);
-        //System.out.println(photo);
-       titre.setText("coucou");
-      //  this.photo.setImage(i);
+        listeIndex=0;
+        titre.setText("coucou");
     }
 
 
     @FXML
-    protected void initCarte(Carte c){
+    protected void CliqueCase(Carte c) {
 
+        if(nbClique<2){
+            nbClique++;
+
+        }else if (nbClique==2){
+            if (CartePrecedent.getFace().getUrl().equals(CarteActuel.getFace().getUrl()) && CartePrecedent!= null &&CarteActuel!=null){
+                CartePrecedent.carteEstValide();
+                CarteActuel.carteEstValide();
+                nbCarteVailde=nbCarteVailde+2;
+                nbClique=1;
+                 if (nbCarteVailde==12){
+                    partieFinie.setVisible(true);
+                 }
+            }else{
+                CartePrecedent.CarteFausse();
+                CarteActuel.CarteFausse();
+                nbClique=1;
+            }
+        }
+
+        c.CarteClique();
+        InitialisationTableau();
+
+        if (CarteActuel != null) {
+            CartePrecedent = CarteActuel;
+        }
+        CarteActuel = c;
+
+       // InitialisationTableau();
     }
+
+
 }
